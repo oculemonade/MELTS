a085b6b32241cf3b7d86e60623301d3577624e51
Nic Litza <91225360+oculemonade@users.noreply.github.com>
2023-01-19 17:59:34 -0500

    Add files via upload

---
+++ go_alphamelts.py
@@ -0,0 +1,7 @@
+##  This program does all the dirty work and starts melts
+##  and loads in the environment and .melts file, which 
+##  be easily changed with proper naming schemes!
+
+import os
+
+os.system('/Users/nic/Desktop/macosx_alphamelts_1-9/bin/run_alphamelts.command -f Sudbury.txt -m Sudbury.melts')

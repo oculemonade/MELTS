eca10121d56240bd3a629a4c4e4bde5aed6aa179
Nic Litza <91225360+oculemonade@users.noreply.github.com>
2023-04-09 21:41:20 -0400

    Update go_alphamelts.py

--- go_alphamelts.py
+++ go_alphamelts.py
@@ -1,7 +1,8 @@
 ##  This program does all the dirty work and starts melts
 ##  and loads in the environment and .melts file, which 
-##  be easily changed with proper naming schemes!
+##  be easily changed with proper naming schemes/paths!
 
 import os
 
-os.system('/Users/nic/Desktop/macosx_alphamelts_1-9/bin/run_alphamelts.command -f Sudbury.txt')
+os.system('/Users/nic/Desktop/alphaMELTS2-master/bin/run-alphamelts.command < batch.txt')
+os.system('python3 next_sim2.py')
